ad34bc51815e2b79e774f98ed57de305afe96093
Todd123178 <todd.taylor.tt@gmail.com>
2014-05-03 21:48:21 -0700

    ch5
    updated the app to chapter 5

--- MyContactList/src/com/example/mycontactlist/ContactActivity.java
+++ MyContactList/src/com/example/mycontactlist/ContactActivity.java
@@ -3,16 +3,19 @@ package com.example.mycontactlist;
 import com.example.mycontactlist.DatePickerDialog.SaveDateListener;
 
 import android.support.v4.app.FragmentActivity;
-
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.text.format.Time;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -21,7 +24,12 @@ import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class ContactActivity extends FragmentActivity implements SaveDateListener{
-    @Override
+	private Contact currentContact;
+	
+	
+	
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
@@ -31,11 +39,17 @@ public class ContactActivity extends FragmentActivity implements SaveDateListene
         initToggleButton();
         setForEditing(false);
         initChangeDateButton();
-        
+        currentContact = new Contact();
+        initTextChangedEvents();
+        initTextChangedEvents();
+        initSaveButton();
     }
 
 
-    @Override
+    
+
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -107,7 +121,7 @@ public class ContactActivity extends FragmentActivity implements SaveDateListene
     	EditText editAddress = (EditText) findViewById(R.id.editAddress );
     	EditText editCity = (EditText) findViewById(R.id.editCity );
     	EditText editState = (EditText) findViewById(R.id.editState );
-    	EditText editZipCode = (EditText) findViewById(R.id.editZipCode );
+    	EditText editZipCode = (EditText) findViewById(R.id.editZipcode );
     	EditText editPhone = (EditText) findViewById(R.id.editHome );
     	EditText editCell = (EditText) findViewById(R.id.editCell);
     	EditText editEmail = (EditText) findViewById(R.id.editEMail );
@@ -130,15 +144,37 @@ public class ContactActivity extends FragmentActivity implements SaveDateListene
     	}
     	else{
     		ScrollView s= (ScrollView) findViewById(R.id.scrollView1);
+    		s.fullScroll(ScrollView.FOCUS_UP);
     		s.clearFocus();
     	}
     }
+   private void hideKeyboard() {
+	   InputMethodManager imm =(InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE );
+	   EditText editName = (EditText) findViewById(R.id.editName );
+	   imm.hideSoftInputFromWindow(editName.getWindowToken(), 0);
+	   EditText editAddress = (EditText) findViewById(R.id.editAddress );
+	   imm.hideSoftInputFromWindow(editAddress.getWindowToken(), 0);
+	   EditText editCity= (EditText) findViewById(R.id.editCity );
+	   imm.hideSoftInputFromWindow(editName.getWindowToken(), 0);
+	   EditText editState = (EditText) findViewById(R.id.editState );
+	   imm.hideSoftInputFromWindow(editAddress.getWindowToken(), 0);
+	   EditText editZipCode = (EditText) findViewById(R.id.editZipcode );
+	   imm.hideSoftInputFromWindow(editName.getWindowToken(), 0);
+	   EditText editHome = (EditText) findViewById(R.id.editHome);
+	   imm.hideSoftInputFromWindow(editAddress.getWindowToken(), 0);
+	   EditText editCell = (EditText) findViewById(R.id.editCell);
+	   imm.hideSoftInputFromWindow(editName.getWindowToken(), 0);
+	   EditText editEmail = (EditText) findViewById(R.id.editEMail);
+	   imm.hideSoftInputFromWindow(editAddress.getWindowToken(), 0);
+	     
+	   }
 
 
 @Override
 public void didFinishDatePickerDialog(Time selectedTime) {
 	TextView birthDay = (TextView) findViewById(R.id.textBirthday);
 	birthDay.setText(DateFormat.format("MM/dd/yyyy", selectedTime.toMillis(false)).toString());
+	currentContact.setBirthday(selectedTime);
 	
 }
 private void initChangeDateButton(){
@@ -153,4 +189,139 @@ private void initChangeDateButton(){
 		}
 	});
 }
+
+private void initSaveButton() {
+	Button saveButton = (Button) findViewById(R.id. buttonSave );
+	saveButton.setOnClickListener( new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			hideKeyboard();
+			ContactDataSource ds = new ContactDataSource(ContactActivity. this ); //1
+			ds.open(); //2
+			boolean wasSuccessful = false ; //3
+			if ( currentContact.getContactID()==-1) { //4
+				wasSuccessful = ds.insertContact( currentContact );
+				int newId =ds.getLastContactId();
+				currentContact.setContactID(newId);
+			}
+			else {
+				wasSuccessful = ds.updateContact( currentContact );
+			}
+			ds.close(); //5
+			if (wasSuccessful) { //6
+				ToggleButton editToggle = (ToggleButton)findViewById(R.id. toggleButtonEdit );
+				editToggle.toggle();
+				setForEditing(false);
+			}
+		}
+});
 }
+private void initTextChangedEvents() {
+	final EditText contactName = (EditText) findViewById(R.id.editName ); 
+	contactName.addTextChangedListener( new TextWatcher() { 
+	public void afterTextChanged(Editable s) { 
+	currentContact .setContactName(contactName.getText().toString());
+	}
+	public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { 
+	// Auto-generated method stub
+	}
+	public void onTextChanged(CharSequence s, int start, int before, int count) { 
+	// Auto-generated method stub
+	}
+	});
+	final EditText Address = (EditText) findViewById(R.id.editAddress ); 
+	Address.addTextChangedListener( new TextWatcher() {
+	public void afterTextChanged(Editable s) {
+	currentContact .setStreetAddress(Address.getText().toString()); 
+	}
+	public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) {
+	// Auto-generated method stub
+	}
+	public void onTextChanged(CharSequence s, int start, int before,int count) {
+	// Auto-generated method stub
+	}
+	});
+	
+
+final EditText City = (EditText) findViewById(R.id.editCity ); 
+contactName.addTextChangedListener( new TextWatcher() { 
+public void afterTextChanged(Editable s) { 
+currentContact.setCity(City.getText().toString());
+}
+public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { //5
+// Auto-generated method stub
+}
+public void onTextChanged(CharSequence s, int start, int before, int count) { //6
+// Auto-generated method stub
+}
+});
+
+final EditText State = (EditText) findViewById(R.id.editState ); 
+contactName.addTextChangedListener( new TextWatcher() { 
+public void afterTextChanged(Editable s) { 
+currentContact.setState(State.getText().toString());
+}
+public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { //5
+// Auto-generated method stub
+}
+public void onTextChanged(CharSequence s, int start, int before, int count) { //6
+// Auto-generated method stub
+}
+});
+
+final EditText zipCode = (EditText) findViewById(R.id.editZipcode ); 
+contactName.addTextChangedListener( new TextWatcher() { 
+public void afterTextChanged(Editable s) { 
+currentContact.setZipCode(zipCode.getText().toString());
+}
+public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { //5
+// Auto-generated method stub
+}
+public void onTextChanged(CharSequence s, int start, int before, int count) { //6
+// Auto-generated method stub
+}
+});
+final EditText Phone = (EditText) findViewById(R.id.editHome ); 
+contactName.addTextChangedListener( new TextWatcher() { 
+public void afterTextChanged(Editable s) { 
+currentContact.setPhoneNumber(Phone.getText().toString());
+}
+public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { //5
+// Auto-generated method stub
+}
+public void onTextChanged(CharSequence s, int start, int before, int count) { //6
+// Auto-generated method stub
+}
+
+});
+final EditText Cell = (EditText) findViewById(R.id.editCell ); 
+contactName.addTextChangedListener( new TextWatcher() { 
+public void afterTextChanged(Editable s) { 
+currentContact.setCellNumber(Cell.getText().toString());
+}
+public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { //5
+// Auto-generated method stub
+}
+public void onTextChanged(CharSequence s, int start, int before, int count) { //6
+// Auto-generated method stub
+}
+});
+final EditText Email = (EditText) findViewById(R.id.editEMail ); 
+contactName.addTextChangedListener( new TextWatcher() { 
+public void afterTextChanged(Editable s) { 
+currentContact.setEMail(Email.getText().toString());
+}
+public void beforeTextChanged(CharSequence arg0, int arg1,int arg2, int arg3) { //5
+// Auto-generated method stub
+}
+public void onTextChanged(CharSequence s, int start, int before, int count) { //6
+// Auto-generated method stub
+}
+});
+
+Cell.addTextChangedListener( new PhoneNumberFormattingTextWatcher());
+Phone.addTextChangedListener( new PhoneNumberFormattingTextWatcher());
+
+}
+}
+
--- MyContactList/src/com/example/mycontactlist/ContactSettingsActivity.java
+++ MyContactList/src/com/example/mycontactlist/ContactSettingsActivity.java
@@ -3,6 +3,8 @@ package com.example.mycontactlist;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -11,9 +13,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.ScrollView;
 import android.os.Build;
 
 public class ContactSettingsActivity extends ActionBarActivity {
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +29,23 @@ public class ContactSettingsActivity extends ActionBarActivity {
 		initListButton();
         initMapButton();        
         initSetttingsButton();
-
-	}
+        initSortByClick();
+        initSortOrderClick();
+        initBackgroundColor();
+        initSettings();
+        View s = (View) findViewById(R.id.page);
+        RadioButton rbRed = (RadioButton) findViewById(R.id.radioRed); 
+    	if(rbRed.isChecked())
+    	{
+    		s.setBackgroundResource(R.color.red);	
+    	}
+    	else
+    	{
+    		s.setBackgroundResource(R.color.blue);
+    	}
+        
+        
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -52,8 +74,6 @@ public class ContactSettingsActivity extends ActionBarActivity {
     			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     			startActivity(intent);
     		}
-			
-			
 		});
     }
     private void initMapButton(){
@@ -64,15 +84,121 @@ public class ContactSettingsActivity extends ActionBarActivity {
     			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     			startActivity(intent);
     		}
-			
-			
 		});
     }
     private void initSetttingsButton(){
     	ImageButton list=(ImageButton) findViewById(R.id.imageButtonSettings);
     	list.setEnabled(false);
     }
+    private void initSettings() {
+    	String sortBy = getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).getString("sortfield","contactname"); //1
+    	String sortOrder = getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).getString("sortorder" ,"asc");
+    	String pickColor = getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).getString("backcolor" ,"red");
+    	
+    	RadioButton rbName = (RadioButton) findViewById(R.id.radioName);
+    	RadioButton rbCity = (RadioButton) findViewById(R.id.radioCity);
+    	RadioButton rbBirthDay = (RadioButton) findViewById(R.id.radioBirthday);
+    	
+    	if (sortBy.equalsIgnoreCase( "contactname" )) { 
+    	rbName.setChecked( true );
+    	}
+    	else if (sortBy.equalsIgnoreCase( "city" )) {
+    	rbCity.setChecked( true );
+    	}
+    	else {
+    	rbBirthDay.setChecked( true );
+    	}
+    	
+    	RadioButton rbAscending = (RadioButton) findViewById(R.id.radioAscending); //4
+    	RadioButton rbDescending = (RadioButton) findViewById(R.id.radioDescending);
+    	
+    	if (sortOrder.equalsIgnoreCase( "asc" )) {
+    	rbAscending.setChecked( true );
+    	}
+    	else {
+    	rbDescending.setChecked( true );
+    	}
+    	
+    	RadioButton rbRed = (RadioButton) findViewById(R.id.radioRed); 
+    	RadioButton rbBlue = (RadioButton) findViewById(R.id.radioBlue);
+    	
+    	if (pickColor.equalsIgnoreCase( "red" ))
+    	{
+    		rbRed.setChecked(true);
+    	}
+    	else
+    	{
+    		rbBlue.setChecked(true);
+    	}
+    	
+    }
+    	
+   
+    private void initSortByClick() {
+    	RadioGroup rgSortBy = (RadioGroup) findViewById(R.id.radioGroup1 );
+    	rgSortBy.setOnCheckedChangeListener( new OnCheckedChangeListener() {
+    	@Override
+    	public void onCheckedChanged(RadioGroup arg0, int arg1) {
+    	RadioButton rbName = (RadioButton) findViewById(R.id.radioName );
+    	RadioButton rbCity = (RadioButton) findViewById(R.id.radioCity );
+    	if (rbName.isChecked()) {
+    	getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "sortfield" , "contactname" ).commit();
+    	}
+    	else if (rbCity.isChecked()) {
+    	getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "sortfield" , "city" ).commit();
+    	}
+    	else {
+    	getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "sortfield" , "birthday" ).commit();
+    	}
+    	}
+    	});
+    }
+    	
 
-	
-
+    private void initSortOrderClick() {
+    	RadioGroup rgSortOrder = (RadioGroup) findViewById(R.id.radioGroup2 );
+    	rgSortOrder.setOnCheckedChangeListener( new OnCheckedChangeListener() {
+    	@Override
+    	public void onCheckedChanged(RadioGroup arg0, int arg1) {
+    	RadioButton rbAscending = (RadioButton)findViewById(R.id.radioAscending );
+    	
+    	if (rbAscending.isChecked()) {    	
+    		getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "sortorder" , "asc" ).commit();
+    	}
+    	else 
+    	{
+    	getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "sortorder" , "desc" ).commit();
+    	}
+    	}
+    	});
+    }
+    private void initBackgroundColor() {
+    	RadioGroup rgSortOrder = (RadioGroup) findViewById(R.id.radioGroup3 );
+    	
+    	rgSortOrder.setOnCheckedChangeListener( new OnCheckedChangeListener() {
+    	@Override
+    	public void onCheckedChanged(RadioGroup arg0, int arg1) {RadioButton rbRed = (RadioButton)
+    	findViewById(R.id.radioRed );
+    	if (rbRed.isChecked()) {
+    	getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "backcolor" , "red" ).commit();
+    	}
+    	else {
+    	getSharedPreferences("MyContactListPreferences",Context.MODE_PRIVATE ).edit()
+    	.putString( "backcolor" , "blue" ).commit();
+    	}
+    	}
+    	});
+    }
+    
 }
+
+
+    	
+
+
